b2626268a130ef7f954e1df41a1214ec242de8cb
VlasikAleksandr <aleksandrVlasik.15@gmail.com>
2022-06-25 20:16:42 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 
 import React from 'react';
 import './App.css';
-import Profile from './components/Profile';
-import Header from './components/Header';
-import Nav from './components/Nav';
+import HeaderContainet from './components/Header/HeaderContainer';
+import Nav from './components/Nav/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import News from './components/News/News';
+import Musik from './components/Musik/Musik';
+import Settings from './components/Settings/Settings';
+import DialogsContainer from './components/Dialogs/DiologsContainer';
+import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
+import Login from './components/Login/Login';
 
-const App = () => {
+
+const App = (props) => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Nav />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <HeaderContainet />
+        <Nav />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/profile' element={<ProfileContainer />} />
+            <Route path='/dialogs' element={<DialogsContainer />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/musik' element={<Musik />} />
+            <Route path='/users' element={<UsersContainer />} />
+            <Route path='/settings' element={<Settings />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter >
   );
 }
 export default App;
